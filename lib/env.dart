2aa2fcbe9a4357a882fb22c3cdeb83a83de75260
AHMETM1 <ahmadmazn8@gmail.com>
2025-09-30 21:25:47 +0300

    Create env.dart

---
+++ lib/env.dart
@@ -0,0 +1,12 @@
+class Env {
+  // 🔹 Shopify mağaza ayarların
+  static const String shopifyStoreUrl = "https://maximalliq.com";
+  static const String shopifyStorefrontAccessToken = "b2dcf187cf1f77946f5a60e37fd6dfda";
+
+  // 🔹 Ortam seçimi (dev / stage / prod)
+  static const String environment = "prod";
+
+  // Eğer ileride farklı ortamlar açarsan buraya ekleyebilirsin
+  // örnek:
+  // static const String environment = "dev";
+}
